23c435ed895488eb9e22865ac125560d72945ddf
Alexis Saettler <alexis@saettler.org>
2019-09-20 00:34:11 +0200

    upgrade aravel-webauthn

--- resources/views/vendor/webauthn/authenticate.blade.php
+++ resources/views/vendor/webauthn/authenticate.blade.php
@@ -12,12 +12,16 @@
     @license MIT
   -->
 
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
   <title>{{ config('app.name', 'Laravel') }}</title>
 
   <!-- Scripts -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="{{ asset('js/app.js') }}" defer></script>
   <script src="{!! secure_asset('vendor/webauthn/webauthn.js') !!}"></script>
 
   <!-- Fonts -->
@@ -32,7 +36,7 @@
     <main class="py-4">
       <div class="container">
         <div class="row">
-          <div class="col-xs-12 col-md-6 col-md-offset-3 col-md-offset-3-right  ">
+          <div class="col-12 col-md-6 offset-md-3 offset-md-3-right  ">
             <div class="card">
               <div class="card-header">{{ trans('webauthn::messages.auth.title') }}</div>
 
