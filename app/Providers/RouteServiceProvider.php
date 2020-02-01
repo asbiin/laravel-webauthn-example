5a3ba8b2a57279495a255183deb800883e173d4f
Alexis Saettler <alexis@saettler.org>
2020-02-01 12:56:44 +0100

    Update dockerfile (#32)

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
@@ -23,9 +24,9 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-
         parent::boot();
+
+        URL::forceScheme('https');
     }
 
     /**
