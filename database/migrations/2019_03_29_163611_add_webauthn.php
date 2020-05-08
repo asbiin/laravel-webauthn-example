96bf27bb2e2ccdacd0639154e05b9cdde4ddc10d
Alexis Saettler <alexis@saettler.org>
2020-05-08 15:41:21 +0200

    fix migration

--- database/migrations/2019_03_29_163611_add_webauthn.php
+++ database/migrations/2019_03_29_163611_add_webauthn.php
@@ -15,7 +15,7 @@ class AddWebauthn extends Migration
     {
         Schema::create('webauthn_keys', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('user_id');
+            $table->unsignedBigInteger('user_id');
 
             $table->string('name')->default('key');
             $table->string('credentialId', 255);
