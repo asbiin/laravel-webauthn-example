df8ecabf497ff582220f36139a7190a4255864c9
Alexis Saettler <alexis@saettler.org>
2022-05-06 19:48:54 +0200

    Add sentry to track issues

---
+++ app/Http/Middleware/SentryContext.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Sentry\State\Scope;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+class SentryContext
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     *
+     * @return mixed
+     *
+     * @codeCoverageIgnore
+     */
+    public function handle($request, Closure $next)
+    {
+        if (app()->bound('sentry')) {
+            \Sentry\configureScope(function (Scope $scope): void {
+                // Add user context
+                $user = Auth::user();
+                if ($user !== null) {
+                    $scope->setUser([
+                        'name' => $user->name,
+                        'email' => $user->email,
+                    ]);
+                }
+                $scope->setTag('page.route.name', Route::currentRouteName() ?? '');
+                $scope->setTag('page.route.action', Route::currentRouteAction() ?? '');
+            });
+        }
+
+        return $next($request);
+    }
+}
