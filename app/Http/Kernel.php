af630ed7a4010bb7303d6f7ea8681b481d5d01ff
Alexis Saettler <alexis@saettler.org>
2022-08-20 23:53:13 +0200

    feat: add plausible (#526)

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,6 +39,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
+            \VincentBean\LaravelPlausible\Middleware\TrackPlausiblePageviews::class,
         ],
 
         'api' => [
