4ba34a566db47e99c92f3dab4087fc5cb83c1eec
Alexis Saettler <alexis@saettler.org>
2023-02-19 12:01:17 +0100

    update

--- app/Listeners/WebauthnRegisterHandler.php
+++ app/Listeners/WebauthnRegisterHandler.php
@@ -2,9 +2,8 @@
 
 namespace App\Listeners;
 
-use App\Notifications\NewKeyRegisteredAlert;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Notification;
 use LaravelWebauthn\Events\WebauthnRegister;
 
 class WebauthnRegisterHandler
@@ -18,6 +17,8 @@ class WebauthnRegisterHandler
     public function handle(WebauthnRegister $event)
     {
         Log::info("Webauthn register: {$event->webauthnKey->name}");
+
+        Cookie::queue('webauthn_remember', $event->webauthnKey->user_id, 60 * 24 * 30);
 
         // if (($notifier = config('mail.notifier')) !== null) {
         //     Notification::route('mail', $notifier)
