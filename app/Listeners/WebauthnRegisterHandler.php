eeb3e230c285cae57f0353ad08d84129a7e6149d
Alexis Saettler <alexis@saettler.org>
2023-02-26 12:05:55 +0100

    feat: add plausible custom events

--- app/Listeners/WebauthnRegisterHandler.php
+++ app/Listeners/WebauthnRegisterHandler.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Log;
 use LaravelWebauthn\Events\WebauthnRegister;
+use VincentBean\LaravelPlausible\PlausibleEvent;
 
 class WebauthnRegisterHandler
 {
@@ -17,6 +18,10 @@ class WebauthnRegisterHandler
     public function handle(WebauthnRegister $event)
     {
         Log::info("Webauthn register: {$event->webauthnKey->name}");
+
+        PlausibleEvent::fire('webauthn-register', [
+            'username' => $event->webauthnKey->user->name
+        ]);
 
         Cookie::queue('webauthn_remember', $event->webauthnKey->user_id, 60 * 24 * 30);
 
