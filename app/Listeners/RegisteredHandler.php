537f1cdf35213879dc7b6f37b50773538c7ceb60
Alexis Saettler <alexis@saettler.org>
2023-02-26 12:13:37 +0100

    feat: add plausible custom events (#645)

--- app/Listeners/RegisteredHandler.php
+++ app/Listeners/RegisteredHandler.php
@@ -6,6 +6,7 @@ use App\Notifications\NewUserAlert;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Notification;
+use VincentBean\LaravelPlausible\PlausibleEvent;
 
 class RegisteredHandler
 {
@@ -19,6 +20,10 @@ class RegisteredHandler
     {
         if ($event->user instanceof \App\Models\User) {
             Log::info("Webauthn register: {$event->user->name} {$event->user->email}");
+
+            PlausibleEvent::fire('register', [
+                'username' => $event->user->name
+            ]);
         }
 
         if (($notifier = config('mail.notifier')) !== null) {
--- app/Listeners/WebauthnLoginHandler.php
+++ app/Listeners/WebauthnLoginHandler.php
@@ -6,6 +6,7 @@ use App\Notifications\KeyLoginAlert;
 use LaravelWebauthn\Events\WebauthnLogin;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Notification;
+use VincentBean\LaravelPlausible\PlausibleEvent;
 
 class WebauthnLoginHandler
 {
@@ -20,6 +21,10 @@ class WebauthnLoginHandler
         $user = $event->user;
         if ($user instanceof \App\Models\User) {
             Log::info("Webauthn login: {$user->name} {$user->email}");
+
+            PlausibleEvent::fire('webauthn-login', [
+                'username' => $user->name
+            ]);
 
             if ($event->eloquent && ($notifier = config('mail.notifier')) !== null) {
                 Notification::route('mail', $notifier)
