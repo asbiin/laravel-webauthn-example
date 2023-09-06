91c830417295fb4c5d6869a4512f05e529958d84
Alexis Saettler <alexis@saettler.org>
2023-09-06 16:32:20 +0200

    chore(deps): update dependencies

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -15,7 +15,7 @@ defineProps({
 
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-800 sm:items-center sm:pt-0">
         <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            <Link v-if="$page.props.user" :href="route('dashboard')" class="text-sm text-gray-700 dark:text-gray-300 underline">
+            <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="text-sm text-gray-700 dark:text-gray-300 underline">
                 Dashboard
             </Link>
 
