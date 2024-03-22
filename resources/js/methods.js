e4a09a85af72fbfee5899a99c6f85ee89acfe86b
Alexis Saettler <alexis@saettler.org>
2024-03-22 23:40:18 +0100

    Add js emitter (#884)

--- resources/js/methods.js
+++ resources/js/methods.js
@@ -1,4 +1,5 @@
 import { computed } from 'vue';
+import emitter from 'tiny-emitter/instance';
 
 /**
  * Get the message in case WebAuthn is not supported.
@@ -10,3 +11,20 @@ export const webAuthnNotSupportedMessage = computed(() =>
     ? 'WebAuthn only supports secure connections. Please load this page with https scheme.'
     : 'Your browser doesn’t currently support WebAuthn.'
 );
+
+/**
+ * Flash a message.
+ *
+ * @param {string} message
+ * @param {string} level
+ */
+export const flash = (message, level = 'success') => {
+  emitter.emit('flash', { message, level });
+};
+
+export default {
+  flash,
+  $on: (...args) => emitter.on(...args),
+  $once: (...args) => emitter.once(...args),
+  $off: (...args) => emitter.off(...args),
+};
